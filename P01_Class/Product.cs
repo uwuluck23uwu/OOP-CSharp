6076d26aeb76cfe5271c56a64bdd9326d010c721
uwuluck23uwu <luck23000000@gmail.com>
2023-10-18 11:21:15 +0700

    List in Class

--- P01_Class/Product.cs
+++ P01_Class/Product.cs
@@ -12,6 +12,23 @@
         //set ใส่ค่า
         
         //=> Lamda, Arrow Function
-        public void Display() => Console.WriteLine($"Id => {Id} \nName => {Name} \nPrice => {Price.ToString("###.##")} \nAmount => {Amount} \n");
+        public void Display() => Console.WriteLine($"Id => {Id} \nName => {Name} \nPrice => {Price.ToString("#,###.##")} \nAmount => {Amount} \n");
+        public List<Product> GenerateProduct(int number = 1)
+        {
+            var products = new List<Product>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                products.Add(new Product
+                {
+                    Id = i + 1,
+                    Name = "Coffee" + (i + 1),
+                    Price = r.NextDouble() * 990 + 10,
+                    Amount = r.Next(10, 50)
+                });
+            }
+            return products;
+        }
     }
 }
--- P01_Class/Program.cs
+++ P01_Class/Program.cs
@@ -1,18 +1,5 @@
 ﻿using P01_Class;
-Random r = new Random();
 
-//List ไม่จำกัดจำนวน
-List<Product> products = new List<Product>();
+new Product().GenerateProduct(10).ForEach(x => x.Display());
 
-for (int i = 0; i < 10; i++)
-{
-    products.Add(new Product 
-    {
-        Id = i+1, 
-        Name = "Coffee"+(i+1), 
-        Price = r.NextDouble() * 990 + 10, 
-        Amount = r.Next(10,50) 
-    });
-}
-
-products.ForEach(p => p.Display());
+//product.GenerateProduct(10).ForEach(x => x.Display());
