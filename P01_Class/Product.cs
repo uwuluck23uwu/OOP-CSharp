2e3d55dbc605c9b1be675f1fceaf75c0f83f58e2
uwuluck23uwu <luck23000000@gmail.com>
2023-10-18 09:55:17 +0700

    Class & OOP

---
+++ P01_Class/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace P01_Class
+{
+    public class Product //แม่แบบ
+    {
+        //attributes, property คุณสมบัติ
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        //Default Method get set
+        //get อ่านค่า
+        //set ใส่ค่า
+    }
+}
---
+++ P01_Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using P01_Class;
+
+//Object วัตถุ
+Product product = new Product();
+Product ton = new Product();
+Product sumo = new Product();
+
+product.Id = 1;
+product.Name = "Coffee";
+product.Price = 10;
+product.Amount = 20;
+
+ton.Id = 2;
+ton.Name = "Ton";
+ton.Price = 10000;
+ton.Amount = 1;
+
+sumo.Id = 3;
+sumo.Name = "Sumo";
+sumo.Price = 10000;
+sumo.Amount = 1;
+
+//Reuse ใช้ซ้ำ
+Display(product);
+Display(ton);
+Display(sumo);
+
+void Display(Product p)
+{
+    Console.WriteLine($"Id => {p.Id} \nName => {p.Name} \nPrice => {p.Price} \nAmount => {p.Amount} \n");
+}
