61e9c1d945eb474669e63a4bb96e55d3de75f4f5
uwuluck23uwu <luck23000000@gmail.com>
2023-11-21 14:28:12 +0700

    Dictionary

---
+++ P08_Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿//Json
+Dictionary<string,string> Country = new Dictionary<string,string>();
+Dictionary<int,double> UnitOfMoney = new Dictionary<int,double>();
+
+Country.Add("Kan", "Kanchanaburi");
+Country.Add("CM","ChaiengMai");
+Country.Add("BKK","BangKok");
+UnitOfMoney.Add(1, 1_000_000);
+UnitOfMoney.Add(2, 2_000_000);
+UnitOfMoney.Add(3, 3_000_000);
+UnitOfMoney.Add(4, 4_000_000);
+UnitOfMoney.Add(5, 5_000_000);
+
+if (Country.ContainsKey("Kan")) Console.WriteLine(Country["Kan"]);
+
+foreach (var item in UnitOfMoney)
+{
+    Console.WriteLine($"{item.Key} {item.Value}");
+}
+
+var total = UnitOfMoney[1] + UnitOfMoney[2];
+UnitOfMoney.Sum(p => p.Value);
